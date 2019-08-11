fe2e6e7f29926c280d2595037ffb2b2eec4b55c2
Bruno Drugowick <bruno.drugowick@gmail.com>
2019-08-11 00:22:20 -0300

    Final version from tutorial.

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -20,7 +20,23 @@ module.exports = {
         }
 
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log("Deu match!");
+            console.log("Deu match!", loggedDev._id, targetDev._id);
+            const loggedUserSocket = req.connectedUsers[loggedDev._id];
+            const likedUserSocket = req.connectedUsers[targetDev._id];
+
+            console.log("logger user socket", loggedUserSocket);
+            if (loggedUserSocket) {
+                req.webSocketServer
+                    .to(loggedUserSocket)
+                    .emit('match', targetDev);
+            }
+
+            console.log("Liked user socket", likedUserSocket);
+            if (likedUserSocket) {
+                req.webSocketServer
+                    .to(likedUserSocket)
+                    .emit('match', loggedDev)
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
